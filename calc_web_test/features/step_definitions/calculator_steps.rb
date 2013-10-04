bc57b74c9035a6bad1b58ab712ae17c6af891023
Lyubomyr Vovk <lvovk@homeaway.com>
2013-10-04 14:05:48 +0300

    web tests skeleton

---
+++ calc_web_test/features/step_definitions/calculator_steps.rb
@@ -0,0 +1,11 @@
+Given(/^the input  "([^"]*)"$/) do |input|
+  pending
+end
+
+When(/^the calculator is run$/) do
+  pending
+end	
+
+Then(/^the output should be "([^"]*)"$/) do |expected_output|
+  pending
+end
